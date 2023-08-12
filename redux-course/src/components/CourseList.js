0262693101dc28a25f4d49c8699c548c9e5c6ab9
A00183340\AR100003 <erennkazan@gmail.com>
2023-08-12 15:43:27 +0300

    css edit and all card list remove

--- redux-course/src/components/CourseList.js
+++ redux-course/src/components/CourseList.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
 import CourseItem from './CourseItem'
+import {clearCart} from '../control/cartSlice'
 
 function CourseList() {
+    const dispatch = useDispatch();
     const { cartItems,quantity,total } = useSelector((store) =>store.cart)
   return (
     <>
@@ -20,7 +22,7 @@ function CourseList() {
             </header>
             <div>
                 {cartItems.map((item)=>{
-                    return <CourseItem {...item}/>
+                    return <CourseItem key={item.id} {...item}/>
                 })}
             </div>
             <footer>
@@ -28,7 +30,7 @@ function CourseList() {
                 <div>
                     <h4>Toplam Tutar<span>{total} TL</span></h4>
                 </div>
-                <button>Temizle</button>
+                <button className='cartClearButton' onClick={()=>dispatch(clearCart())}>Temizle</button>
             </footer>
         </section>
     )}
